078c5dcdf55b4932220146ea4413f5359e5c3bed
Mario Valentino <valentinomario05@gmail.com>
2024-08-29 16:33:45 +0200

    Fixed distance estimation error

--- circular_distance.m
+++ circular_distance.m
@@ -1,8 +1,11 @@
 function d = circular_distance(q1,q2)
-    %q1=mod(q1,2*pi);
-    %q2=mod(q2,2*pi);
+    q1=mod(q1,2*pi);
+    q2=mod(q2,2*pi);
 
     d_bar = (mod(q2 - q1, 2*pi));
     d = min([d_bar; 2*pi-d_bar]);
 end
+
+
+
 
--- coverage_parameters.m
+++ coverage_parameters.m
@@ -4,8 +4,8 @@ close all
 n = 5; % sensors number
 lambda = [2.4; 2.8; 3.0; 4.2; 3.6]; % max sensor velocity
 %lambda = [0.1, 0.2, 0.3, 0.6, 0.8]';
-%lambda = [1; 1; 1; 1; 1];
-delta = [1.2, 4.0, 1.6, 3.0, 1.8]; % measurement error upper bound
+lambda = 3*[1; 1; 1; 1; 1];
+delta = [1.2, 4.0, 1.6, 3.0, 1.8]*0; % measurement error upper bound
 
 delta_bar = zeros(n,n);
 
@@ -24,7 +24,7 @@ phi = [pi/6, pi/3, pi/2, pi/4, pi/5]; % error phase
 
 q0 = sort(rand(1, n) * 2 * pi)'; % random position init
 
-Tf = 2000;
-T_star = pi/sum(lambda)
-
+Tf = 200;
+T_star = pi/sum(lambda);
+fprintf('Parameters updated %s\n', (datetime))
 
